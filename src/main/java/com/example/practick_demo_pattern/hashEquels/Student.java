ff8602a2adf873b69cebeddf6c56ec1350056a13
ukoval400 <ukoval400@gmail.com>
2024-12-18 14:00:21 +0100

    update Student equals

--- src/main/java/com/example/practick_demo_pattern/hashEquels/Student.java
+++ src/main/java/com/example/practick_demo_pattern/hashEquels/Student.java
@@ -1,6 +1,10 @@
 package com.example.practick_demo_pattern.hashEquels;
 
+import java.util.Objects;
+
 public class Student {
+
+    private String id;
     private String name;
 
     public Student(String name) {
@@ -11,10 +15,16 @@ public class Student {
     public boolean equals(Object obj) {
         // Сравнение ссылок
         // Проверка типа
+        // Создать обект и присвоить ему обект который передали в парметрах
         // Сравнение содержимого
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
-        Student student = (Student) obj;
-        return name.equals(student.name);
+        Student s = (Student) obj;
+        return name.equals(s.name) && id.equals(((Student) obj).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
